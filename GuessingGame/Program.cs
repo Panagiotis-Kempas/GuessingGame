ceb48a0038b6471f4793fdfa64208435d058d8f0
panos <panos@LAPTOP-5I8OJ1AJ>
2021-11-21 19:21:52 +0200

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am thinking of a number between 1 and 10.What is it?");
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 11);
+            int guess;
+            int numberOfGuesses = 3;
+            bool gameOver = false;
+            
+
+
+
+
+
+
+                while (gameOver == false)
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    numberOfGuesses--;
+
+                    if (guess != randomNumber && numberOfGuesses == 0)
+                    {
+                        Console.WriteLine("You had 3 guesses and did not find the number.YOU LOSE!");
+                        gameOver = true;
+                    }
+                    else if (guess == randomNumber)
+                    {
+                        Console.WriteLine("Thats it!YOU WIN!");
+                        gameOver = true;
+                    }
+                    else if (guess > randomNumber)
+                    {
+                        Console.WriteLine("Nope!My number is smaller than that.");
+                    }
+                    else if (guess < randomNumber)
+                    {
+                        Console.WriteLine("NO.My number is greater than that.");
+                    }
+                }
+
+                
+                
+            }
+        }
+    }
+}
